a5bd60defd1ed7e7fc402bf0214d5afca429d705
Justin Lecas <justin.lecas@gmail.com>
2022-01-04 10:56:19 +0100

    BDD

---
+++ README.md
@@ -0,0 +1,12 @@
+### Pour accéder a la base via phpmyadmin
+
+- localhost:8080
+- serveur : db
+- user : root
+- password :
+
+### Dans le cas ou vous avez besoin de générer le script DB
+
+- cd db/scripts
+- source venv/bin/activate activate
+- ./generate-insert.py
--- db/init.sql
+++ db/init.sql
@@ -1,228 +1,147 @@
--- -----------------------------------------------------------------------------
---             G�n�ration d'une base de donn�es pour
---                      Oracle Version 9.xx
---                        (31/12/2021 12:54:24)
--- -----------------------------------------------------------------------------
---      Nom de la base : MLR2
---      Projet : 
---      Auteur : Diallo
---      Date de derni�re modification : 31/12/2021 12:54:20
--- -----------------------------------------------------------------------------
+DROP DATABASE IF EXISTS skystar;
 
-DROP TABLE CONSTELLATION CASCADE CONSTRAINTS;
+CREATE DATABASE IF NOT EXISTS skystar;
+USE skystar;
 
-DROP TABLE JEU CASCADE CONSTRAINTS;
+CREATE TABLE IF NOT EXISTS CONSTELLATION
+ (
+   ID_CONSTELLATION INTEGER(8) NOT NULL AUTO_INCREMENT  ,
+   LATIN_NAME CHAR(50) NULL  ,
+   OBSERVATION_SAISON CHAR(100) NULL  ,
+   ETOILE_PRINCIPALE CHAR(40) NULL  ,
+   RA DECIMAL(10,5) NULL  ,
+   DECA DECIMAL(10,5) NULL  ,
+   TAILLE DECIMAL(15,5) NULL  ,
+   CREATED DATETIME NULL  ,
+   UPDATED DATETIME NULL  
+   , PRIMARY KEY (ID_CONSTELLATION) 
+ ) 
+ comment = "";
 
-DROP TABLE OBJET_DISTANT CASCADE CONSTRAINTS;
-
-DROP TABLE OBJET_PROCHE CASCADE CONSTRAINTS;
-
-DROP TABLE PARCOURS CASCADE CONSTRAINTS;
-
-DROP TABLE GROUPER CASCADE CONSTRAINTS;
-
-DROP TABLE DETERMINER CASCADE CONSTRAINTS;
-
--- -----------------------------------------------------------------------------
---       CREATION DE LA BASE 
--- -----------------------------------------------------------------------------
-
-CREATE DATABASE MLR2;
-
--- -----------------------------------------------------------------------------
---       TABLE : CONSTELLATION
--- -----------------------------------------------------------------------------
-
-CREATE TABLE CONSTELLATION
-   (
-    ID_CONSTELLATION NUMBER(8)  NOT NULL,
-    LATIN_NAME CHAR(50)  NULL,
-    OBSERVATION_SAISON CHAR(50)  NULL,
-    ETOILE_PRINCIPALE CHAR(40)  NULL,
-    RA CHAR(32)  NULL,
-    DECA CHAR(32)  NULL,
-    TAILLE NUMBER(15,5)  NULL,
-    CREATED DATE  NULL,
-    UPDATED DATE  NULL
-,   CONSTRAINT PK_CONSTELLATION PRIMARY KEY (ID_CONSTELLATION)  
-   ) ;
-
--- -----------------------------------------------------------------------------
---       TABLE : JEU
--- -----------------------------------------------------------------------------
-
-CREATE TABLE JEU
-   (
-    ID_JEU NUMBER(5)  NOT NULL,
-    ID_CONSTELLATION NUMBER(8)  NULL,
-    ID_OBJET_DISTANT NUMBER(5)  NULL,
-    PSEUDO CHAR(20)  NULL,
-    TROUVER NUMBER(1)  NULL,
-    DUREE DATE  NULL,
-    DATE_CREATION DATE  NULL,
-    CREATED DATE  NULL,
-    UPDATED DATE  NULL
-,   CONSTRAINT PK_JEU PRIMARY KEY (ID_JEU)  
-   ) ;
-
--- -----------------------------------------------------------------------------
---       INDEX DE LA TABLE JEU
--- -----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS JEU
+ (
+   ID_JEU INTEGER(5) NOT NULL  AUTO_INCREMENT,
+   ID_CONSTELLATION INTEGER(8) NULL  ,
+   ID_OBJET_DISTANT INTEGER(5) NULL  ,
+   PSEUDO CHAR(20) NULL  ,
+   TROUVER TINYINT(1) NULL  ,
+   DUREE DATETIME NULL  ,
+   DATE_CREATION DATETIME NULL  ,
+   CREATED DATETIME NULL  ,
+   UPDATED DATETIME NULL  
+   , PRIMARY KEY (ID_JEU) 
+ ) 
+ comment = "";
 
 CREATE  INDEX I_FK_JEU_CONSTELLATION
-     ON JEU (ID_CONSTELLATION ASC)
-    ;
+     ON JEU (ID_CONSTELLATION ASC);
 
 CREATE  INDEX I_FK_JEU_OBJET_DISTANT
-     ON JEU (ID_OBJET_DISTANT ASC)
-    ;
+     ON JEU (ID_OBJET_DISTANT ASC);
 
--- -----------------------------------------------------------------------------
---       TABLE : OBJET_DISTANT
--- -----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS OBJET_DISTANT
+ (
+   ID_OBJET_DISTANT INTEGER(5) NOT NULL AUTO_INCREMENT ,
+   RA DECIMAL(10,5) NULL  ,
+   DECA DECIMAL(10,5) NULL  ,
+   MAGNITUDE REAL(10,3) NULL  ,
+   RA_RADIANS REAL(10,5) NULL  ,
+   DEC_RADIANS REAL(10,5) NULL  ,
+   TYPE CHAR(50) NULL  ,
+   CREATED DATETIME NULL  ,
+   UPDATED DATETIME NULL  
+   , PRIMARY KEY (ID_OBJET_DISTANT) 
+ ) 
+ comment = "";
 
-CREATE TABLE OBJET_DISTANT
-   (
-    ID_OBJET_DISTANT NUMBER(5)  NOT NULL,
-    RA NUMBER  NULL,
-    DECA NUMBER  NULL,
-    MAGNITUDE NUMBER  NULL,
-    RA_RADIANS NUMBER  NULL,
-    DEC_RADIANS NUMBER  NULL,
-    TYPE CHAR(50)  NULL,
-    CREATED DATE  NULL,
-    UPDATED DATE  NULL
-,   CONSTRAINT PK_OBJET_DISTANT PRIMARY KEY (ID_OBJET_DISTANT)  
-   ) ;
+CREATE TABLE IF NOT EXISTS OBJET_PROCHE
+ (
+   ID_OBJET_PROCHE INTEGER(5) NOT NULL AUTO_INCREMENT ,
+   NOM CHAR(32) NULL  ,
+   MAGNITUDE REAL(10,5) NULL  ,
+   RA DECIMAL(10,5) NULL  ,
+   DECA DECIMAL(10,5) NULL  ,
+   TYPE CHAR(32) NULL  ,
+   DATE_APPROBATION CHAR(32) NULL  
+   , PRIMARY KEY (ID_OBJET_PROCHE) 
+ ) 
+ comment = "";
 
--- -----------------------------------------------------------------------------
---       TABLE : OBJET_PROCHE
--- -----------------------------------------------------------------------------
-
-CREATE TABLE OBJET_PROCHE
-   (
-    ID_OBJET_PROCHE NUMBER(5)  NOT NULL,
-    NOM CHAR(32)  NULL,
-    MAGNITUDE NUMBER  NULL,
-    RA NUMBER  NULL,
-    DECA NUMBER  NULL,
-    TYPE CHAR(32)  NULL,
-    DATE_APPROBATION CHAR(32)  NULL
-,   CONSTRAINT PK_OBJET_PROCHE PRIMARY KEY (ID_OBJET_PROCHE)  
-   ) ;
-
--- -----------------------------------------------------------------------------
---       TABLE : PARCOURS
--- -----------------------------------------------------------------------------
-
-CREATE TABLE PARCOURS
-   (
-    ID_PARCOURS NUMBER(5)  NOT NULL,
-    ID_JEU NUMBER(5)  NOT NULL,
-    RA NUMBER  NULL,
-    DECA NUMBER  NULL,
-    MAGNITUDE NUMBER  NULL,
-    CREATED DATE  NULL,
-    UPDATED DATE  NULL
-,   CONSTRAINT PK_PARCOURS PRIMARY KEY (ID_PARCOURS)  
-   ) ;
-
--- -----------------------------------------------------------------------------
---       INDEX DE LA TABLE PARCOURS
--- -----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS PARCOURS
+ (
+   ID_PARCOURS INTEGER(5) NOT NULL AUTO_INCREMENT  ,
+   ID_JEU INTEGER(5) NOT NULL  ,
+   RA DECIMAL(10,5) NULL  ,
+   DECA DECIMAL(10,5) NULL  ,
+   MAGNITUDE REAL(10,5) NULL  ,
+   CREATED DATETIME NULL  ,
+   UPDATED DATETIME NULL  
+   , PRIMARY KEY (ID_PARCOURS) 
+ ) 
+ comment = "";
 
 CREATE  INDEX I_FK_PARCOURS_JEU
-     ON PARCOURS (ID_JEU ASC)
-    ;
+     ON PARCOURS (ID_JEU ASC);
 
--- -----------------------------------------------------------------------------
---       TABLE : GROUPER
--- -----------------------------------------------------------------------------
-
-CREATE TABLE GROUPER
-   (
-    ID_OBJET_DISTANT NUMBER(5)  NOT NULL,
-    ID_CONSTELLATION NUMBER(8)  NOT NULL
-,   CONSTRAINT PK_GROUPER PRIMARY KEY (ID_OBJET_DISTANT, ID_CONSTELLATION)  
-   ) ;
-
--- -----------------------------------------------------------------------------
---       INDEX DE LA TABLE GROUPER
--- -----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS GROUPER
+ (
+   ID_OBJET_DISTANT INTEGER(5) NOT NULL AUTO_INCREMENT  ,
+   ID_CONSTELLATION INTEGER(8) NOT NULL  
+   , PRIMARY KEY (ID_OBJET_DISTANT,ID_CONSTELLATION) 
+ ) 
+ comment = "";
 
 CREATE  INDEX I_FK_GROUPER_OBJET_DISTANT
-     ON GROUPER (ID_OBJET_DISTANT ASC)
-    ;
+     ON GROUPER (ID_OBJET_DISTANT ASC);
 
 CREATE  INDEX I_FK_GROUPER_CONSTELLATION
-     ON GROUPER (ID_CONSTELLATION ASC)
-    ;
+     ON GROUPER (ID_CONSTELLATION ASC);
 
--- -----------------------------------------------------------------------------
---       TABLE : DETERMINER
--- -----------------------------------------------------------------------------
-
-CREATE TABLE DETERMINER
-   (
-    ID_OBJET_PROCHE NUMBER(5)  NOT NULL,
-    ID_CONSTELLATION NUMBER(8)  NOT NULL
-,   CONSTRAINT PK_DETERMINER PRIMARY KEY (ID_OBJET_PROCHE, ID_CONSTELLATION)  
-   ) ;
-
--- -----------------------------------------------------------------------------
---       INDEX DE LA TABLE DETERMINER
--- -----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS DETERMINER
+ (
+   ID_OBJET_PROCHE INTEGER(5) NOT NULL AUTO_INCREMENT ,
+   ID_CONSTELLATION INTEGER(8) NOT NULL  
+   , PRIMARY KEY (ID_OBJET_PROCHE,ID_CONSTELLATION) 
+ ) 
+ comment = "";
 
 CREATE  INDEX I_FK_DETERMINER_OBJET_PROCHE
-     ON DETERMINER (ID_OBJET_PROCHE ASC)
-    ;
+     ON DETERMINER (ID_OBJET_PROCHE ASC);
 
 CREATE  INDEX I_FK_DETERMINER_CONSTELLATION
-     ON DETERMINER (ID_CONSTELLATION ASC)
-    ;
+     ON DETERMINER (ID_CONSTELLATION ASC);
+
+ALTER TABLE JEU 
+  ADD FOREIGN KEY FK_JEU_CONSTELLATION (ID_CONSTELLATION)
+      REFERENCES CONSTELLATION (ID_CONSTELLATION) ;
 
 
--- -----------------------------------------------------------------------------
---       CREATION DES REFERENCES DE TABLE
--- -----------------------------------------------------------------------------
+ALTER TABLE JEU 
+  ADD FOREIGN KEY FK_JEU_OBJET_DISTANT (ID_OBJET_DISTANT)
+      REFERENCES OBJET_DISTANT (ID_OBJET_DISTANT) ;
 
 
-ALTER TABLE JEU ADD (
-     CONSTRAINT FK_JEU_CONSTELLATION
-          FOREIGN KEY (ID_CONSTELLATION)
-               REFERENCES CONSTELLATION (ID_CONSTELLATION))   ;
-
-ALTER TABLE JEU ADD (
-     CONSTRAINT FK_JEU_OBJET_DISTANT
-          FOREIGN KEY (ID_OBJET_DISTANT)
-               REFERENCES OBJET_DISTANT (ID_OBJET_DISTANT))   ;
-
-ALTER TABLE PARCOURS ADD (
-     CONSTRAINT FK_PARCOURS_JEU
-          FOREIGN KEY (ID_JEU)
-               REFERENCES JEU (ID_JEU))   ;
-
-ALTER TABLE GROUPER ADD (
-     CONSTRAINT FK_GROUPER_OBJET_DISTANT
-          FOREIGN KEY (ID_OBJET_DISTANT)
-               REFERENCES OBJET_DISTANT (ID_OBJET_DISTANT))   ;
-
-ALTER TABLE GROUPER ADD (
-     CONSTRAINT FK_GROUPER_CONSTELLATION
-          FOREIGN KEY (ID_CONSTELLATION)
-               REFERENCES CONSTELLATION (ID_CONSTELLATION))   ;
-
-ALTER TABLE DETERMINER ADD (
-     CONSTRAINT FK_DETERMINER_OBJET_PROCHE
-          FOREIGN KEY (ID_OBJET_PROCHE)
-               REFERENCES OBJET_PROCHE (ID_OBJET_PROCHE))   ;
-
-ALTER TABLE DETERMINER ADD (
-     CONSTRAINT FK_DETERMINER_CONSTELLATION
-          FOREIGN KEY (ID_CONSTELLATION)
-               REFERENCES CONSTELLATION (ID_CONSTELLATION))   ;
+ALTER TABLE PARCOURS 
+  ADD FOREIGN KEY FK_PARCOURS_JEU (ID_JEU)
+      REFERENCES JEU (ID_JEU) ;
 
 
--- -----------------------------------------------------------------------------
---                FIN DE GENERATION
--- -----------------------------------------------------------------------------
+ALTER TABLE GROUPER 
+  ADD FOREIGN KEY FK_GROUPER_OBJET_DISTANT (ID_OBJET_DISTANT)
+      REFERENCES OBJET_DISTANT (ID_OBJET_DISTANT) ;
+
+
+ALTER TABLE GROUPER 
+  ADD FOREIGN KEY FK_GROUPER_CONSTELLATION (ID_CONSTELLATION)
+      REFERENCES CONSTELLATION (ID_CONSTELLATION) ;
+
+
+ALTER TABLE DETERMINER 
+  ADD FOREIGN KEY FK_DETERMINER_OBJET_PROCHE (ID_OBJET_PROCHE)
+      REFERENCES OBJET_PROCHE (ID_OBJET_PROCHE) ;
+
+
+ALTER TABLE DETERMINER 
+  ADD FOREIGN KEY FK_DETERMINER_CONSTELLATION (ID_CONSTELLATION)
+      REFERENCES CONSTELLATION (ID_CONSTELLATION) ;
+
