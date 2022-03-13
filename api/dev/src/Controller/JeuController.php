c102ba811309ba5dfb954d93c4243fce9019a8bb
Justin Lecas <justin.lecas@gmail.com>
2022-03-13 14:16:54 +0100

    Ajout d'un parcour a un jeu

--- api/dev/src/Controller/JeuController.php
+++ api/dev/src/Controller/JeuController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Jeu;
+use App\Entity\Parcours;
 use App\Repository\JeuRepository;
 use App\Repository\ObjetDistantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
@@ -16,6 +18,42 @@ use DateTimeImmutable;
 #[Route('/api/jeu', name: 'api_jeu_')]
 class JeuController extends AbstractController
 {
+    /**
+     * Ajouter un parcour
+     */
+    #[Route('/{id}/add_parcour', name: 'add_parcour', methods:['POST'])]
+    public function add_parcour(Jeu $jeu, Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        if(!$user || str_replace(" ", "", $jeu->getPseudo()) != str_replace(" ", "", $user->getUserIdentifier()))
+            throw new UnauthorizedHttpException("Vous devez être connecter");
+
+        $json_request = json_decode(
+            $request->getContent(),
+            true
+        );
+        $ra = key_exists("ra", $json_request) ? $json_request["ra"] : null;
+        $deca = key_exists("deca", $json_request) ? $json_request["deca"] : null;
+        $magnitude = key_exists("magnitude", $json_request) ? $json_request["magnitude"] : null;
+
+        if(!is_numeric($ra) 
+            || !is_numeric($deca)
+            || !is_numeric($magnitude))
+            return $this->json(["message" => "RA, DECA et Magnitude doivent être renseigner."], 401, [], ['groups' => 'read:collection']);
+    
+
+        $parcour = new Parcours();
+        $parcour->setIdJeu($jeu);
+        $parcour->setRa($ra);
+        $parcour->setDeca($deca);
+        $parcour->setMagnitude($magnitude);
+
+        $em->persist($parcour);
+        $em->flush();
+        
+        return $this->json($parcour, 201);
+    }
+
     /**
      * Récupère les 10 meilleurs joueurs du classement
      */
